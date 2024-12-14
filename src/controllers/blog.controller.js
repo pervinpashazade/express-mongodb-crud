0a2e362a634ad97f30b1f09d48ceb1e53f11b4ac
pervinpashazade <pervinpashazade2@gmail.com>
2024-12-14 17:50:57 +0400

    multer single upload

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -4,7 +4,22 @@ import jwt from "jsonwebtoken";
 import { appConfig } from "../consts.js";
 import { User } from "../models/user.model.js";
 
+// const count = 4;
+
+// if (isNaN(count) === false) {
+//     res.status(200).json(count)
+// } else {
+//     res.status(500).json(count)
+// }
+
 const create = async (req, res, next) => {
+
+    // if (!req.file) return res.status(400).json({
+    //     message: "Image required!",
+    // })
+
+    // return res.json(req.body) 
+
     const user = req.user;
 
     if (!user.isVerifiedEmail) return res.status(400).json({
@@ -14,7 +29,11 @@ const create = async (req, res, next) => {
     const { title, description } = await Joi.object({
         title: Joi.string().trim().min(3).max(50).required(),
         description: Joi.string().trim().min(10).max(1000).required(),
-    }).validateAsync(req.body, { abortEarly: false })
+        img: Joi.object().required(),
+    }).validateAsync({
+        ...req.body,
+        img: req.file,
+    }, { abortEarly: false })
         .catch(err => {
             return res.status(422).json({
                 message: "Xeta bash verdi!",
@@ -26,6 +45,7 @@ const create = async (req, res, next) => {
         title,
         description,
         user: user.id,
+        img_path: req.file.filename,
     })
         .then(newBlog => res.status(201).json(newBlog))
         .catch(error => res.status(500).json({
@@ -36,7 +56,7 @@ const create = async (req, res, next) => {
 
 const getList = async (req, res, next) => {
     try {
-        const list = await Blog.find().populate("user", "fullname").select("_id title user")
+        const list = await Blog.find().populate("user", "fullname").select("_id title user img_path")
 
         // res.json(list.map(item => {
         //     return {
--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -1,12 +1,54 @@
 import { Router } from "express"
 import { BlogController } from "../controllers/blog.controller.js"
 import { useAuth } from "../middlewares/auth.middleware.js"
+import multer from "multer";
+import { appConfig } from "../consts.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        // const filename = Date.now() + "-" + file.originalname;
+        const filename = `${Date.now()}-${file.originalname}`;
+        cb(null, filename)
+    }
+})
+
+// const upload = multer({ dest: 'uploads/' })
+
+const upload = multer({
+    storage,
+    fileFilter: (_, file, cb) => {
+
+        const arr = [1, 2, 3, 4]
+        const searchResult = arr.find(item => item === 15)
+        // [].every(item => item === 15)
+        // [].some(item => item === 15)
+
+        // const searchResultIndex = arr.indexOf(499) // 3 -> -1
+        const num = 0
+        if (num) {
+            // ...
+        } else {
+            // ...
+        }
+
+        // if (file.mimetype === "image/jpg" || file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        // if (appConfig.allowedImageTypes.includes(file.mimetype)) {
+        if (appConfig.allowedImageTypes.indexOf(file.mimetype) !== -1) {
+            cb(null, true)
+        } else {
+            cb(new Error("File type must be jpeg or png"), false)
+        }
+    }
+})
 
 export const blogRoutes = Router()
 const controller = BlogController()
 
 // -> /api/blog/create
-blogRoutes.post("/create", useAuth, controller.create)
+blogRoutes.post("/create", useAuth, upload.single('img'), controller.create)
 
 // -> /api/blog/list
 blogRoutes.get("/list", controller.getList)
